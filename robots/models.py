e8bf783834cc26d2ee303cf3251e16293118b7f9
Mikhail <lobodamisha29@gmail.com>
2024-12-11 00:45:09 +0100

    Main commit

---
+++ robots/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+
+class Robot(models.Model):
+    serial = models.CharField(max_length=5, blank=False, null=False)
+    model = models.CharField(max_length=2, blank=False, null=False)
+    version = models.CharField(max_length=2, blank=False, null=False)
+    created = models.DateTimeField(blank=False, null=False)
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['serial']),
+            models.Index(fields=['created']),
+        ]
+
+    def __str__(self):
+        return f'{self.created} {self.serial}'
